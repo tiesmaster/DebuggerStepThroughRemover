8a0530a412f63ad47f6c4b217e76c6badb41cfbe
Thijs Brobbel <t.brobbel@coolblue.nl>
2015-08-10 13:31:38 +0200

    Split up analyzer, and code fixer unit tests.

---
+++ DebuggerStepThroughRemover/DebuggerStepThroughRemover.Test/AnalyzerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TestHelper;
+
+namespace DebuggerStepThroughRemover.Test
+{
+    [TestClass]
+    public class AnalyzerTests : DiagnosticVerifier
+    {
+        [TestMethod]
+        public void WithEmptySourceFile_ShouldNotFindAnything()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithImportedNameSpace_ShouldReportAttribute()
+        {
+            var test = @"
+using System.Diagnostics;
+
+namespace ConsoleApplication1
+{
+    [DebuggerStepThrough]
+    class TypeName
+    {   
+    }
+}";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DebuggerStepThroughRemover",
+                Message = $"Type 'TypeName' is decorated with DebuggerStepThrough attribute",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 6, 5)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DebuggerStepThroughRemoverAnalyzer();
+        }
+    }
+}
