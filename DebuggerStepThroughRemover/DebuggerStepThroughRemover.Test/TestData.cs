d1272271951c90811f2e07655f4e32189f5a039d
Thijs Brobbel <t.brobbel@coolblue.nl>
2015-08-17 13:18:40 +0200

    Put in TODO item.

--- DebuggerStepThroughRemover/DebuggerStepThroughRemover.Test/TestData.cs
+++ DebuggerStepThroughRemover/DebuggerStepThroughRemover.Test/TestData.cs
@@ -5,6 +5,8 @@ namespace DebuggerStepThroughRemover.Test
         public string Description { private get; set; }
         public string BrokenSource { get; set; }
         public string ExpectedFixedSource { get; set; }
+        // TODO: maybe replace these two props with some higher level construct
+        // from Rosly itself (maybe Microsoft.CodeAnalysis.Text.LinePosition?)
         public int Line { get; set; }
         public int Column { get; set; }
 
