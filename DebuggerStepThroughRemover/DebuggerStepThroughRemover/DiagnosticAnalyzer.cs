f7f9085f71c57428a4fe628f3930d860ad5a50a5
Thijs Brobbel <thijsbrobbel@gmail.com>
2015-08-18 23:06:54 +0200

    Change analyzer to work on the semantic model,
    instead of the syntax tree.

--- DebuggerStepThroughRemover/DebuggerStepThroughRemover/DiagnosticAnalyzer.cs
+++ DebuggerStepThroughRemover/DebuggerStepThroughRemover/DiagnosticAnalyzer.cs
@@ -1,13 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Text;
 
 namespace DebuggerStepThroughRemover
 {
@@ -30,57 +30,90 @@ namespace DebuggerStepThroughRemover
             DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning,
             isEnabledByDefault: true, description: Description);
 
-        private static readonly string _debuggerStepThroughAttributeName =
-            nameof(DebuggerStepThroughAttribute).Replace(nameof(Attribute), string.Empty);
-
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeClassDeclarationSyntax, SyntaxKind.ClassDeclaration);
+            context.RegisterSemanticModelAction(AnalyzeSemanticModel);
         }
 
-        private static void AnalyzeClassDeclarationSyntax(SyntaxNodeAnalysisContext context)
+        private static void AnalyzeSemanticModel(SemanticModelAnalysisContext context)
         {
-            var classDeclarationNode = (ClassDeclarationSyntax)context.Node;
+            var model = context.SemanticModel;
+            var debuggerStepThroughAttributeSymbol = GetDebuggerStepThroughAttributeSymbol(model);
+            var matchingAttributes = FindMatchingNodes(model, debuggerStepThroughAttributeSymbol);
+            CreateDiagnosticsFromMatchingNodes(context, matchingAttributes);
+        }
 
-            foreach (var debuggerStepThroughAttribute in CreateQuery(classDeclarationNode))
+        private static IEnumerable<AttributeSyntax> FindMatchingNodes(SemanticModel model, INamedTypeSymbol debuggerStepThroughAttributeSymbol)
+        {
+            return
+                from attributeNode in GetAllAttributeNodesInModel(model)
+                let attributeClassSymbol = GetAttributeClassSymbolFromModel(model, attributeNode)
+                where debuggerStepThroughAttributeSymbol.Equals(attributeClassSymbol)
+                select attributeNode;
+        }
+
+        private static INamedTypeSymbol GetDebuggerStepThroughAttributeSymbol(SemanticModel model)
+        {
+            var debuggerStepThroughAttributeType = typeof (DebuggerStepThroughAttribute);
+            var mscorlib = GetMscorlib(model, debuggerStepThroughAttributeType.GetTypeInfo().Assembly);
+            var assemblySymbol = (IAssemblySymbol) model.Compilation.GetAssemblyOrModuleSymbol(mscorlib);
+            return assemblySymbol.GetTypeByMetadataName(debuggerStepThroughAttributeType.FullName);
+        }
+
+        private static MetadataReference GetMscorlib(SemanticModel model, Assembly mscorlibAssembly)
+        {
+            var mscorlibAssemblyName = mscorlibAssembly.GetName().Name;
+            return model.Compilation.References.Single(x => x.Display.Contains(mscorlibAssemblyName));
+        }
+
+        private static IEnumerable<AttributeSyntax> GetAllAttributeNodesInModel(SemanticModel model)
+        {
+            var syntaxRoot = model.SyntaxTree.GetRoot();
+            return syntaxRoot.DescendantNodes().OfType<AttributeSyntax>();
+        }
+
+        private static ITypeSymbol GetAttributeClassSymbolFromModel(SemanticModel model, AttributeSyntax attributeNode)
+        {
+            var attributeSymbolInfo = model.GetSymbolInfo(attributeNode);
+            var constructorSymbol = (IMethodSymbol) attributeSymbolInfo.Symbol;
+            return constructorSymbol.ReceiverType;
+        }
+
+        private static void CreateDiagnosticsFromMatchingNodes(SemanticModelAnalysisContext context, IEnumerable<AttributeSyntax> matchingNodes)
+        {
+            foreach (var matchingNode in matchingNodes)
             {
-                ReportDiagnostic(context, debuggerStepThroughAttribute);
+                ReportDiagnostic(context, matchingNode);
             }
         }
 
-        private static IEnumerable<AttributeSyntax> CreateQuery(ClassDeclarationSyntax classDeclarationNode)
-        {
-            return classDeclarationNode
-                .DescendantNodes()
-                .OfType<AttributeSyntax>()
-                .Where(IsDebuggerStepThroughAttribute);
-        }
-
-        private static bool IsDebuggerStepThroughAttribute(AttributeSyntax attributeNode) =>
-            attributeNode.Name.GetText().ToString().EndsWith(_debuggerStepThroughAttributeName);
-
-        private static void ReportDiagnostic(SyntaxNodeAnalysisContext context, AttributeSyntax attributeSyntax)
+        private static void ReportDiagnostic(SemanticModelAnalysisContext context, AttributeSyntax matchingNode)
         {
             context.ReportDiagnostic(
                 Diagnostic.Create(Rule,
-                    DetermineDiagnosticTarget(attributeSyntax).GetLocation(),
-                    GetClassName(attributeSyntax)));
+                    DetermineDiagnosticTarget(matchingNode).GetLocation(),
+                    GetClassName(matchingNode)));
         }
 
-        private static string GetClassName(AttributeSyntax attributeSyntax)
+        private static string GetClassName(AttributeSyntax attributeNode)
         {
-            var classNode = attributeSyntax.Ancestors().OfType<ClassDeclarationSyntax>().Single();
+            var classNode = attributeNode.Ancestors().OfType<ClassDeclarationSyntax>().Single();
             return classNode.Identifier.Text;
         }
 
         private static CSharpSyntaxNode DetermineDiagnosticTarget(AttributeSyntax attributeNode)
         {
             var attributesParentNode = (AttributeListSyntax)attributeNode.Parent;
-            return attributesParentNode.Attributes.Count > 1
+            return ShouldKeepSquareBrackets(attributesParentNode)
                 ? (CSharpSyntaxNode) attributeNode
                 : attributesParentNode;
+        }
+
+        private static bool ShouldKeepSquareBrackets(AttributeListSyntax attributesParentNode)
+        {
+            return attributesParentNode.Attributes.Count > 1;
         }
     }
 }
